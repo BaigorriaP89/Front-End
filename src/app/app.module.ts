569f35f06783b68066a1a93fb5df5bff3b6f001a
BaigorriaP89 <pablo.baigorria_89@hotmail.com>
2022-05-14 20:21:51 -0300

    reinicio de todo para ordenar componentes en carpetas y css ponerlo en raiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './Componentes/cabecera/cabecera.component';
+import { AcercaDeComponent } from './Componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './Componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
